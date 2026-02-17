97e31b8a82abd3ba44cdb9a6ce1c8b24d7b02650
WINBIGFOX <winbigfox@t-online.de>
2026-02-17 17:13:17 +0100

    add: implement OpenedFromURL event listener to handle URL-based timestamp actions

---
+++ app/Listeners/OpenedFromURL.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use App\Services\TimestampService;
+use Illuminate\Support\Uri;
+use Native\Desktop\Events\App\OpenedFromURL as OpenedFromURLEvent;
+
+class OpenedFromURL
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(OpenedFromURLEvent $event): void
+    {
+        $url = Uri::of($event->url);
+        if ($url->host() === 'start') {
+            switch ($url->path()) {
+                case 'work': TimestampService::startWork();
+                    break;
+                case 'break': TimestampService::startBreak();
+                    break;
+            }
+        }
+        if ($url->host() === 'stop') {
+            TimestampService::stop();
+        }
+    }
+}
